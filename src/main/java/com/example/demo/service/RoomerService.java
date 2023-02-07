02271aae28dad7da717406845d7fe587516d3999
Laptop <stark2542@gmail.com>
2023-02-07 22:14:37 +0200

    Update: save method and added web page for save roomer

--- src/main/java/com/example/demo/service/RoomerService.java
+++ src/main/java/com/example/demo/service/RoomerService.java
@@ -5,13 +5,18 @@ import com.example.demo.repo.RoomerRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
 public class RoomerService {
 
     private final RoomerRepo roomerRepo;
+    private Date currentDate;
 
     public void saveRoomer(Roomer roomer) {
+        currentDate = new Date();
+        roomer.setSettlementDate(currentDate);
         roomerRepo.save(roomer);
     }
 }
