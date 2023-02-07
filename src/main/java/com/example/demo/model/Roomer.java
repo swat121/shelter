a9c73a9bd46370f98a5b089cd985f80a4bc06bbd
Laptop <stark2542@gmail.com>
2023-02-07 21:25:34 +0200

    Add: classes for data base

--- src/main/java/com/example/demo/model/Roomer.java
+++ src/main/java/com/example/demo/model/Roomer.java
@@ -1,7 +1,27 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import java.util.Date;
 
 @Data
+@Entity
 public class Roomer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String secondName;
+    private String firstName;
+    private String surname;
+    private String dateOfBirth;
+    private String numberOfPassport;
+    private int passportIssuedBy;
+    private String phoneNumber;
+    private String placeOfArrival;
+    private int roomNumber;
+    @Temporal(TemporalType.DATE)
+    private Date settlementDate;
+    @Temporal(TemporalType.DATE)
+    private Date moveOutDate;
 }
--- src/main/java/com/example/demo/repo/RoomerRepo.java
+++ src/main/java/com/example/demo/repo/RoomerRepo.java
@@ -1,7 +1,9 @@
 package com.example.demo.repo;
 
+import com.example.demo.model.Roomer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RoomerRepo {
+public interface RoomerRepo extends JpaRepository<Long, Roomer> {
 }
