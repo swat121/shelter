cfe4d9d224f5038bacafd78b3c1a1c6e64228a85
swat121 <stark2542@gmail.com>
2023-02-10 21:43:18 +0200

    Fix: roomer model

--- src/main/java/com/example/demo/model/Roomer.java
+++ src/main/java/com/example/demo/model/Roomer.java
@@ -1,9 +1,8 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.Date;
 
@@ -13,32 +12,32 @@ public class Roomer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "{NotEmpty.Roomer}")
+    @NotBlank(message = "{NotEmpty.Roomer}")
     private String secondName;
 
-    @NotEmpty(message = "{NotEmpty.Roomer}")
+    @NotBlank(message = "{NotEmpty.Roomer}")
     private String firstName;
 
-    @NotEmpty(message = "{NotEmpty.Roomer}")
+    @NotBlank(message = "{NotEmpty.Roomer}")
     private String surname;
 
-    @NotEmpty(message = "{NotEmpty.Roomer}")
+    @NotBlank(message = "{NotEmpty.Roomer}")
     private String dateOfBirth;
 
-    @NotEmpty(message = "{NotEmpty.Roomer}")
+    @NotBlank(message = "{NotEmpty.Roomer}")
     private String numberOfPassport;
 
     @NotNull(message = "{NotNull.Roomer}")
-    private int passportIssuedBy;
+    private Integer passportIssuedBy;
 
     @NotNull(message = "{NotEmpty.Roomer}")
     private String phoneNumber;
 
-    @NotEmpty(message = "{NotEmpty.Roomer}")
+    @NotBlank(message = "{NotEmpty.Roomer}")
     private String placeOfArrival;
 
     @NotNull(message = "{NotNull.Roomer}")
-    private int roomNumber;
+    private Integer roomNumber;
 
     @Temporal(TemporalType.DATE)
     private Date settlementDate;
